077db69d2ff202d268e8a6b8360c7386bc8e5842
mechandoqc <mechando37@gmail.com>
2026-05-18 00:55:21 -0400

    Make ready event async and send a welcome message
    Added async functionality to the ready event and send a message to a specific channel.

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-client.once('ready', () => {
+client.once('ready', async () => {
     console.log(`Bot connecté : ${client.user.tag}`);
+
+    const channel = await client.channels.fetch("1505632960236355724");
+
+    channel.send("Méchando est connecté 🤖");
 });
 
 client.login(process.env.TOKEN);
